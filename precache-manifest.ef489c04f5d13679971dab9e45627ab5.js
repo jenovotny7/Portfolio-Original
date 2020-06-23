bdf142be052c3b4aa4f8c804d3bf12b179d7aaf0
Jack Novotny <jenovotny7@gmail.com>
2020-06-23 15:33:15 -0500

    Updates

---
+++ precache-manifest.ef489c04f5d13679971dab9e45627ab5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "789cd237e6b34a37ccbaef2a36b9526a",
+    "url": "/jnovotny/index.html"
+  },
+  {
+    "revision": "d70512acc0483f901908",
+    "url": "/jnovotny/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "5364dc458c9eea3ad1f7",
+    "url": "/jnovotny/static/js/2.72773549.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/jnovotny/static/js/2.72773549.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d70512acc0483f901908",
+    "url": "/jnovotny/static/js/main.1855c368.chunk.js"
+  },
+  {
+    "revision": "865ae455f97cdd4ca008",
+    "url": "/jnovotny/static/js/runtime-main.d4e88765.js"
+  }
+]);
